02fb608e806e3366eeb79be013229dc9cff1b132
Paul Glass <paul.glass@rackspace.com>
2015-08-14 10:37:21 -0500

    init

---
+++ src/main/java/StoolMetrics.java
@@ -0,0 +1,12 @@
+import org.sonar.api.measures.Metric;
+import org.sonar.api.measures.Metrics;
+
+import java.util.List;
+
+public class StoolMetrics implements Metrics {
+
+    @Override
+    public List<Metric> getMetrics() {
+        return null;
+    }
+}
